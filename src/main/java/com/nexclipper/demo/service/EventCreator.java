95439a044ccfd7e6c5462e5ea0ef64d0a72d5573
Sarang Anmulwar <sanmulwar@calibo.com>
2021-11-17 22:14:23 +0530

    configurable

--- src/main/java/com/nexclipper/demo/service/EventCreator.java
+++ src/main/java/com/nexclipper/demo/service/EventCreator.java
@@ -17,29 +17,40 @@ public class EventCreator {
     @Value("${application.text}")
     String sampleLog;
 
+    @Value("${application.logSize}")
+    String logSize;
+
     @Scheduled(fixedRate = 1000)
     public void create1() {
-        int size = 30;
-        while(size>0){
-            log.info(sampleLog);
-            size--;
-        }
+        logToConsole(0);
     }
 
     @Scheduled(fixedRate = 1000)
     public void create2() {
-        int size = 30;
-        while(size>0){
-            log.info(sampleLog+sampleLog);
-            size--;
-        }
+       logToConsole(1);
     }
 
     @Scheduled(fixedRate = 1000)
     public void create3() {
-        int size = 50;
+        logToConsole(2);
+//        int size = 50;
+//        while(size>0){
+//            log.info(sampleLog +"Unsafe statement written to the binary log using statement format since BINLOG_FORMAT = STATEMENT. The statement is unsafe because it uses a LIMIT clause. This is unsafe because the set of rows included cannot be predicted.Slave SQL thread exiting, replication stopped in log 'dbserver-2-bin.000033''.");
+//            size--;
+//        }
+    }
+
+    private void logToConsole(int position){
+        String log1 = logSize.split(",")[position];
+        int size = Integer.parseInt(log1.split("\\|")[0]);
+        int multiple = Integer.parseInt(log1.split("\\|")[1]);
         while(size>0){
-            log.info(sampleLog +"Unsafe statement written to the binary log using statement format since BINLOG_FORMAT = STATEMENT. The statement is unsafe because it uses a LIMIT clause. This is unsafe because the set of rows included cannot be predicted.Slave SQL thread exiting, replication stopped in log 'dbserver-2-bin.000033''.");
+            String logStr = "";
+            while(multiple>0){
+                logStr=logStr+sampleLog;
+                multiple--;
+            }
+            log.info(logStr);
             size--;
         }
     }
